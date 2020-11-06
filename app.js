2b07c0c152b12c12d6557c6ed1ea75479a8b13a8
Meruyert Karim <meruyert.karim@nu.edu.kz>
2020-11-06 15:54:51 +0100

    Array

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
 var person = {
     name: "Mika",
-    age: 27
+    age: 27,
+    hobbies: ['Spotrs', 'Cooking']
 };
-console.log(person.nickname);
+var favoriteActivities;
+favoriteActivities = ['sport'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -4,7 +4,16 @@
 // } = {
     const person = {
     name: "Mika",
-    age: 27
+    age: 27, 
+    hobbies: ['Spotrs', 'Cooking']
 }
 
+let favoriteActivities: string[];
+favoriteActivities = ['sport'];
+
 console.log(person.name);
+
+for(const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); !!! ERROR!! !
+}

c2a1a60ffb02bb688510d5d92066d18688ad1893
Meruyert Karim <meruyert.karim@nu.edu.kz>
2020-11-07 12:39:34 +0100

    Tupl

--- app.js
+++ app.js
@@ -5,12 +5,16 @@
 var person = {
     name: "Mika",
     age: 27,
-    hobbies: ['Spotrs', 'Cooking']
+    hobbies: ['Spotrs', 'Cooking'],
+    role: [2, 'author']
 };
+person.role.push('admin');
+person.role[1] = 10;
 var favoriteActivities;
 favoriteActivities = ['sport'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    console.log(hobby);
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); !!! ERROR!! !
 }
--- app.ts
+++ app.ts
@@ -2,11 +2,23 @@
 //     name: string;
 //     age: number;
 // } = {
-    const person = {
+    const person: {
+        name: string;
+        age: number;
+        hobbies: string[];
+        role:[number, string] //tuple;
+    } = {
     name: "Mika",
     age: 27, 
-    hobbies: ['Spotrs', 'Cooking']
-}
+    hobbies: ['Spotrs', 'Cooking'],
+    role: [2, 'author']
+};
+
+
+//person.role.push('admin');
+//person.role[1] = 10;
+
+//person.role = [0, 'admin', 'user'];
 
 let favoriteActivities: string[];
 favoriteActivities = ['sport'];
